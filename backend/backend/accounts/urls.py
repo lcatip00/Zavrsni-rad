4bcfdb095a0d2dac77c2ade43dd83cead10c4a86
lcatip00 <lcatip00@fesb.hr>
2021-07-18 20:05:54 +0200

    remove unnecessary urls from accounts

--- backend/backend/accounts/urls.py
+++ backend/backend/accounts/urls.py
@@ -1,9 +1,7 @@
 from .views import (
     AccountCreateAPIView,
     AccountDetailAPIView,
-    AccountListAPIView,
     AccountUpdateAPIView,
-    GetAccountAPIView,
     MyAccountDetailsView,
     PatchProfileImageView
 )
@@ -16,9 +14,7 @@ urlpatterns = [
     path('myprofile/', MyAccountDetailsView.as_view(), name='myprofile'),
     path('create/', AccountCreateAPIView.as_view(), name='create'),
     path('<slug>/', AccountDetailAPIView.as_view(), name='details'),
-    path('', AccountListAPIView.as_view(), name='list'),
     path('<slug>/update/', AccountUpdateAPIView.as_view(), name='update'),
-    path('user/<int:id>', GetAccountAPIView.as_view(), name='user'),
     path('changeProfileImage/<int:id>/', PatchProfileImageView.as_view(), name='changeProfileImage')
 
 ]
