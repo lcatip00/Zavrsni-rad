021e3e52e51ea16c84a2d6a293ad20ba68dd7c5f
lcatip00 <lcatip00@fesb.hr>
2021-07-28 22:31:18 +0200

    Add unfollow btn

--- frontend/frontend/src/components/FollowCategory.js
+++ frontend/frontend/src/components/FollowCategory.js
@@ -1,7 +1,14 @@
 import axios from 'axios'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const FollowCategory = (props) => {
+    const [isFollower, setIsFollower]  = useState();
+    
+    
+    useEffect(() => {
+        // check if user is follower
+        checkIsFollower();
+    }, [isFollower]);
 
     const handleClick = () => {
         const token = localStorage.getItem('token');
@@ -19,11 +26,32 @@ const FollowCategory = (props) => {
         }
     }
 
+    const checkIsFollower = () => {
+        const token = localStorage.getItem('token');
+        axios.get(`http://127.0.0.1:8000/categories/isFollower/${props.slug}/`, {headers: {
+            'Authorization': `Token ${token}`}
+        }).then(res =>{
+            setIsFollower(res.data.follower)
+            console.log("res za foll: ", res.data.follower)
+        }).catch(err => {console.log("eror je: ",err)})
+    }
+
     return(     
         <div>
-            <button type="button" className="btn btn-secondary btn-sm mt-2 mr-1" onClick={handleClick}>
-                Follow
-            </button>
+            {/* if user is follower show btn unfollow */}
+            {
+                isFollower == "true" && 
+                    <button type="button" className="btn btn-secondary btn-sm mt-2 mr-1" onClick={handleClick}>
+                        Unfollow 
+                    </button>       
+            }
+            {/* if user is not follower show btn follow */}
+            {
+                isFollower == "false" && 
+                    <button type="button" className="btn btn-secondary btn-sm mt-2 mr-1" onClick={handleClick}>
+                        Follow
+                    </button>
+            }           
         </div>
     )
 }
