4de30a4900a1baaf523dbfa69bfaa8a99804a685
lcatip00 <lcatip00@fesb.hr>
2021-09-05 14:34:42 +0200

    Add confirmation modal

--- frontend/frontend/src/components/DeleteCommetnButton.js
+++ frontend/frontend/src/components/DeleteCommetnButton.js
@@ -6,7 +6,7 @@ const DeleteCommentButton = (props) =>{
     const handleClick = () => {
         const token = localStorage.getItem('token');
 
-        axios.delete(`http://127.0.0.1:8000/comments/delete/${props.id}`,{headers: {
+        axios.delete(`${process.env.REACT_APP_BASE_URL}/comments/delete/${props.id}`,{headers: {
             'Authorization': `Token ${token}` 
           }
         }).then(res => {
@@ -18,9 +18,31 @@ const DeleteCommentButton = (props) =>{
     }
 
     return(
-        <div className="float-right">
-            <button className="btn btn-secondary btn-sm mt-2 float-right" onClick={handleClick}>Delete comment</button>
-        </div>
+        <div className="float-right ms-auto mr-2 ">    
+            <button type="button" className="btn btn-secondary btn-sm mr-1 mb-2" data-toggle="modal" data-target="#exampleModal">
+                Delete Comment
+            </button>
+
+            <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Delete Comment</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            Are you sure you want to delete this comment?
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                            <button type="button" className="btn btn-primary" onClick={handleClick} data-dismiss="modal">Delete</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>  
     )
 }
 
--- frontend/frontend/src/components/DeletePostButton.js
+++ frontend/frontend/src/components/DeletePostButton.js
@@ -8,7 +8,7 @@ const DeletePostButton = (props) =>{
 
     const handleClick = () => {
         const token = localStorage.getItem('token');
-        axios.delete(`http://127.0.0.1:8000/posts/delete/${props.slug}`,{headers: {
+        axios.delete(`${process.env.REACT_APP_BASE_URL}/posts/delete/${props.slug}`,{headers: {
             'Authorization': `Token ${token}` 
           }
         }).then(res => {
@@ -22,10 +22,34 @@ const DeletePostButton = (props) =>{
         return <Redirect to="/"></Redirect>
     }
 
-    return(
-        <div className="float-right ms-auto mr-2 ">
-            <button className="btn btn-secondary btn-sm mr-1 mb-2" onClick={handleClick}>Delete Post</button>
-        </div>
+    return(  
+        <div className="float-right ms-auto mr-2 ">    
+            <button type="button" className="btn btn-secondary btn-sm mr-1 mb-2" data-toggle="modal" data-target="#exampleModal">
+                Delete Post
+            </button>
+
+
+            <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Delete Post</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            Are you sure you want to delete this post?
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                            <button type="button" className="btn btn-primary" onClick={handleClick} data-dismiss="modal">Delete</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>  
     )
 }
 
