17e488855bc5e33619042d3ea80b4910c6463ae1
lcatip00 <lcatip00@fesb.hr>
2021-09-05 14:50:34 +0200

    Remove unnecessary code

--- frontend/frontend/src/hoc/Layout.js
+++ frontend/frontend/src/hoc/Layout.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import{Container} from 'react-bootstrap';
 
-// const Layout = (props) => (
-//         {/* <NavBar></NavBar> */}
-//         <Container>
-//             {props.children}
-//         </Container>
-// );
-
-// export default Layout;
-
 const Layout = (props) => (
     <Container>
         {props.children}
